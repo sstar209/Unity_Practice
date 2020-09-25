fc96f0e20087ed6cd75c45752c1f662c87d93518
sstar209 <68266654+sstar209@users.noreply.github.com>
2020-09-25 09:53:41 +0900

    플레이어 총구 추가

---
+++ Assets/Scripts/HeroFirePos_Gizmo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroFirePos_Gizmo : MonoBehaviour
+{
+    public Color color1 = Color.yellow;
+    public float Gizmo_Radius = 0.08f;
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = color1;
+        Gizmos.DrawSphere(transform.position, Gizmo_Radius);
+    }
+}
