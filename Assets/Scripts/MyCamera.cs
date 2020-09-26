1c801c703a5e1b7ec40f7899061e445483169c1c
sstar209 <68266654+sstar209@users.noreply.github.com>
2020-09-26 23:33:42 +0900

    플레이어를 따라 움직이는 카메라 수정

---
+++ Assets/Scripts/MyCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCamera : MonoBehaviour
+{
+    Vector3 offset;
+
+    public float cameraSpeed;   //카메라 이동 속도
+    public Transform target;    //플레이어 오브젝트의 좌표값 저장
+
+    void Start()
+    {
+        //카메라와 플레이어 사이의 거리 구함
+        offset = transform.position - target.position;
+    }
+   
+    void Update()
+    {
+        target = GameObject.Find("Player").GetComponent<Transform>();
+        Vector3 pos = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime * cameraSpeed);
+
+        transform.position = pos;
+    }
+}
