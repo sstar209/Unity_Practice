a05cbdab4fcaf3cf47eb8aa1d9d09eaa00887f31
sstar209 <68266654+sstar209@users.noreply.github.com>
2020-09-27 16:52:33 +0900

    몬스터 자동생성 및 생성지역 기즈모

---
+++ Assets/Scripts/MonsterSpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawn : MonoBehaviour
+{
+    public GameObject monster;
+    public float timeGap = 3f;
+    public Transform[] spawnPositions;  //spawn할 지점은 몇 군데?
+
+    void Start()
+    {
+        //timeGap(3초)만큼 시간이 지난 후 최초로 SpawnMonster 함수를 호출하고
+        //시간이 timeGap만큼 지날 떄마다 반복 호출
+
+        InvokeRepeating("SpawnMonster", timeGap, timeGap);
+    }
+
+    void SpawnMonster()
+
+    {
+        //몬스터가 나오는 장소를 랜덤으로 설정
+        int spawnIndex = Random.Range(0, spawnPositions.Length);
+
+        //Instantiate 함수를 사용하여 Prefabs폴더에 있는 Rabbit를 생성하여 Scene에 배치
+        Instantiate(monster, spawnPositions[spawnIndex].position, spawnPositions[spawnIndex].rotation);
+    }
+}
---
+++ Assets/Scripts/Spawn_Gizmos.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Gizmos : MonoBehaviour
+{
+    public Color color1 = Color.blue;
+    //기즈모 색을 파란색으로
+
+    public float Gizmo_Radius = 0.2f;
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = color1;
+        Gizmos.DrawSphere(transform.position, Gizmo_Radius);
+    }
+}
